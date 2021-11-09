6c4dab9222e1e44a1d3ddeca85692bc2480dd012
alfybenny <ab8483@princeton.edu>
2021-11-09 17:04:16 -0500

    nothing much

--- wfn.py
+++ wfn.py
@@ -9,18 +9,39 @@ import os.path
 V, x, L, n, i, h, m = symbols('V x L n i h m')
 
 L = 1
+n = 4
+#V = x**2
 
 def psi(i, x):
     return sqrt(2/L)*sin(i*pi*(x+L/2)/L)
 
-#psi = sqrt(2/L)*sin(i*pi*(x+L/2)/L)
 def H_phi(i,x):
-    return -(h/2*m)*diff(psi(i,x),x,x)-(V*psi(i,x))
+    return -(h/2*m)*diff(psi(i,x),x,x)+(V*psi(i,x))
 
-#Hphi = -(h/2*m)*diff(psi,x,x) - V*psi
+def dotproduct(i, j):
+    return integrate(psi(i, x)*H_phi(j, x), (x, -1, 1))
+
+range = [1, 2, 3];
 
 
+def f(i, j):
+    return dotproduct(i+1, j+1)
 
-print(H_phi(2,x))
+def basis(i, j):
+    return psi(i+1, j+1)
 
+
+M = Matrix(n, n, f)
+
+B = Matrix(n, n, basis)
+
+print(M)
+#print(dotproduct(1,1))
+E = M.eigenvals()
+V = M.eigenvects()
+
+wfn = V*B
+
+
+print(wfn)
 
